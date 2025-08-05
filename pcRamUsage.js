233880818762baae376f456451a78ff5bcadedbb
unknown <dev.pedro_sebben@outlook.com.br>
2025-08-05 19:50:26 -0300

    create packed.json for projects future for front-end and functions filter from MB

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,1 +1,20 @@
 const os = require('os');
+
+setInterval(() =>{
+const { arch, plataform, totalmem, freemen} = os;
+const tRam = totalmem() / 1024 / 1024;
+const fRam = freemen() / 1024 / 1024;
+
+
+
+const stats = {
+    OS: plataform(),
+    Arch: arch(),
+    TotalRAM: `$parseInt(tRam) MB`,
+    FreeRAM:`$parseInt(fRam)MB`,
+    Usage: `${usage.toFixed(2)}%`
+}
+console.clear();
+console.table(stats);
+},1000);
+
